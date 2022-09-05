790bc321234ee14a6cffb0affddd9ac4d1a7d63d
Karol <kurekdev@gmail.com>
2022-09-05 19:46:07 +0200

    Create and Read(list/ind) Contact init

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add-contact/', views.add_contact, name='add-contact'),
+    path('profile/<id:pk>/', views.contact_profile, name='contact-profile'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Contact
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    contacts = Contact.objects.all()
+    return render(request, 'index.html', {
+        'contacts': contacts
+    })
+
+
+def add_contact(request):
+    if request.method == 'POST':
+        data = request.POST
+        new_contact = Contact(
+            full_name=data.get('fullname'),
+            relationship=data.get('relationship'),
+            email=data.get('email'),
+            phone_number=data.get('phone-number'),
+            address=data.get('address')
+        )
+        new_contact.save()
+
+        return redirect('/')
+
+    return render(request, 'new.html')
+
+
+def contact_profile(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'contact-profile.html', {
+        'contact': contact
+    })
